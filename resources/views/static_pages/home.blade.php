dd7c83b5ff4d18a1368f2ec4df7c146e1634d2cd
zhennank <zhennank@163.com>
2019-06-25 01:24:09 +0800

    Initial blog

---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.default')
+@section('title','Home')
+@section('content')
+<div class="jumbotron">
+  <h1>Hello Laravel</h1>
+  <p class="lead">
+    Laravel
+  </p>
+  <p>
+    一切，将从这里开始。
+  </p>
+  <p>
+    <a class="btn btn-lg btn-success" href="#" role="button">Now Sign up!</a>
+  </p>
+</div>
+@stop
