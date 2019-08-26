b6f29b06b1a0e6a42b1cd8b885657564321a1fd8
zhennank <zhennankee@gmail.com>
2019-08-26 18:16:18 +0800

    build the basic page

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+ <div class="container">
+   <a class="navbar-brand" href="/">Blog</a>
+   <ul class="navbar-nav justify-content-end">
+     <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">Help</a></li>
+     <li class="nav-item" ><a class="nav-link" href="#">Login</a></li>
+   </ul>
+ </div>
+</nav>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,17 +5,10 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
   <body>
-  	 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
-      <div class="container">
-        <a class="navbar-brand" href="/">Blog</a>
-        <ul class="navbar-nav justify-content-end">
-          <li class="nav-item"><a class="nav-link" href="/help">Help</a></li>
-          <li class="nav-item" ><a class="nav-link" href="#">Login</a></li>
-        </ul>
-      </div>
-    </nav>
+    @include('layouts._header')
     <div class="container">
-      @yield('content')
+    @yield('content')
+    @include('layouts._footer')
     </div>
   </body>
 </html>
