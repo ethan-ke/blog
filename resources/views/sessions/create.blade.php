841f1fb51ef6acd50e9c42722dbcbfa0a6f68bac
zhennank <zhennankee@gmail.com>
2019-09-02 16:44:42 +0800

    Add Session

---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.default')
+@section('title', 'Signin')
+
+@section('content')
+<div class="offset-md-2 col-md-8">
+  <div class="card ">
+    <div class="card-header">
+      <h5>Signin</h5>
+    </div>
+    <div class="card-body">
+      @include('shared._errors')
+
+      <form method="POST" action="{{ route('login') }}">
+          {{ csrf_field() }}
+
+          <div class="form-group">
+            <label for="email">Email：</label>
+            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="password">Password：</label>
+            <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+          </div>
+
+          <button type="submit" class="btn btn-primary">Signin</button>
+      </form>
+
+      <hr>
+
+      <p>New friend？<a href="{{ route('signup') }}">Now Signup！</a></p>
+    </div>
+  </div>
+</div>
+@stop
