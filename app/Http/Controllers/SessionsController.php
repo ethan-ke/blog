42f85b753cc36979d4a5f4de51b962b8171ab501
zhennank <zhennankee@gmail.com>
2019-09-02 17:08:06 +0800

    User logout

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -26,4 +26,11 @@ class SessionsController extends Controller
         return redirect()->back()->withInput();
       }
     }
+
+    public function destroy()
+    {
+      Auth::logout();
+      session()->flash('success','Successful logout!');
+      return redirect('login');
+    }
 }
