8fe2d29a66385dab6932b8f6985bb779b97c7f6a
zhennank <zhennankee@gmail.com>
2019-09-02 15:58:53 +0800

    Error message

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,6 +8,7 @@
       <h5>Signup</h5>
     </div>
     <div class="card-body">
+      @include('shared._errors')
       <form method="POST" action="{{ route('users.store') }}">
           <div class="form-group">
             <label for="name">Name：</label>
