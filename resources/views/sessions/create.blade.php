b49495ec23ea000b9d3094e2586d2d1aeabf2d8c
zhennank <zhennankee@gmail.com>
2019-09-04 13:55:40 +0800

    Reset password

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -19,7 +19,8 @@
           </div>
 
           <div class="form-group">
-            <label for="password">Password：</label>
+            <label for="password">Password:</label>
+            <label for="password">(<a href="{{ route('password.request') }}">Forget password</a>)</label>
             <input type="password" name="password" class="form-control" value="{{ old('password') }}">
           </div>
 
