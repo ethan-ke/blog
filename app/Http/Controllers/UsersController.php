4c60fbdbebc860946e34da9d7350e666f8cd6f70
zhennank <zhennankee@gmail.com>
2019-09-05 11:26:48 +0800

    Home blog list

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Mail;
 use Auth;
+
 class UsersController extends Controller
 {
     public function __construct()
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,18 +2,21 @@
 @section('title','Home')
 @section('content')
   @if (Auth::check())
-    <div class="row">
-      <div class="col-md-8">
-        <section class="status_form">
-          @include('shared._status_form')
-        </section>
-      </div>
-      <aside class="col-md-4">
-        <section class="user_info">
-          @include('shared._user_info', ['user' => Auth::user()])
-        </section>
-      </aside>
+  <div class="row">
+    <div class="col-md-8">
+      <section class="status_form">
+        @include('shared._status_form')
+      </section>
+      <h4>Article list</h4>
+      <hr>
+      @include('shared._feed')
     </div>
+    <aside class="col-md-4">
+      <section class="user_info">
+        @include('shared._user_info', ['user' => Auth::user()])
+      </section>
+    </aside>
+  </div>
   @else
   <div class="jumbotron">
     <h1>Hello Laravel</h1>
